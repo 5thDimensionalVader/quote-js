5ec7d1442c984b76874dc7c3d0ba52d65b509352
jdotcole233 <jdotcole233@gmail.com>
2021-06-29 11:01:50 +0000

    modification to output authors

--- index.js
+++ index.js
@@ -9,20 +9,19 @@ const inputEnd = prompt('Hello, enter any of the following [random, today & quot
 // set the baseURL
 const baseURL = `https://zenquotes.io/api/${inputEnd}`;
 // create the date using the Moment package
-if (inputEnd === 'RANDOM' || inputEnd === 'TODAY' || inputEnd === 'QUOTES' || inputEnd === 'QUOTE' || inputEnd === 'quote') {
-  console.log(`Invalid input => ${inputEnd}.`);
-} else {
-  axios.get(baseURL)
-    .then((res) => {
-      const dateMoment = moment().format('MMMM Do YYYY, h:mm:ss a');
-      res.data.forEach((quote, index) => {
-        console.log(`\n
-          Author: ${quote.a}\n
-          Famous Quote: ${quote.q}\n
-          Date: ${dateMoment}\n
-          `)
-      })
-    }).catch((err) => {
-      console.log(err);
-    });
-}
+
+axios.get(baseURL)
+  .then((res) => {
+    // const dateMoment = moment().format('MMMM Do YYYY, h:mm:ss a');
+    console.log(res.data.map(quote => quote.a))
+    // res.data.forEach((quote, index) => {
+    //   console.log(`\n
+    //     Author: ${quote.a}\n
+    //     Famous Quote: ${quote.q}\n
+    //     Date: ${dateMoment}\n
+    //     `)
+    // })
+  }).catch((err) => {
+    console.log(err);
+  });
+
